9d29b3ef0458b1923a1abddbdd0498a8238c3c94
Anh Ngô Thị Tuyết <19521198@gm.uit.edu.vn>
2020-07-15 11:33:16 +0700

    11.33

---
+++ Project/source.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+#include <ctime>
+#include <iomanip>
+#include <string.h>
+
+#include <io.h>
+#include <fcntl.h>
+#include <string>
+#include <windows.h>
+#include <mmsystem.h>
+
+
+#pragma comment(lib, "winmm.lib")
+
+void gotoXY(int x, int y);
+
+void changColor(int k);
+
+void resizeConsole(int width, int height);
+
+void setFontSize(int FontSize);
